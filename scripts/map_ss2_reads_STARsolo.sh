4cfdd9747f82d38807144fb4d5f1c40d51e29def
cartal <cntalaveralopez@icloud.com>
2021-11-10 17:16:12 +0100

    Added mapping script

---
+++ scripts/map_ss2_reads_STARsolo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+### Map reads to GRCm38 using STARsolo. 
+
+### Set up 
+
+CPU=8
+INDEX=/Volumes/TIGERII/nobackup/refseq/starindex/
+READS=/Users/ctl/github/scBRIE2/data/
+MANIFEST=/Users/ctl/github/scBRIE2/data/manifest.tsv
+SAMPLES=/Users/ctl/github/scBRIE2/data/sample_list.txt
+
+### Run mapping 
+
+for i in $(cat $SAMPLES)
+
+do
+
+    mkdir $i
+
+    cd $i 
+
+    STAR --runThreadN $CPU --genomeDir $INDEX --readFilesIn $READS/$i\_R1.fastq.gz $READS/$i\_R2.fastq.gz --soloType SmartSeq --readFilesManifest $MANIFEST --soloUMIdedup Exact --soloStrand Unstranded --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $i
+
+    cd ..
+
+done
