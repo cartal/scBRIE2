23912e2acfa7a5ec72f3cabed5cc3d640f8406eb
cartal <cntalaveralopez@icloud.com>
2021-11-10 15:10:45 +0100

    Added bash script to generate reference index

---
+++ scripts/generate_reference_index.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+#### SCRIPT TO GENERATE THE REFERENCE INDEX FOR STARsolo
+
+### Computer set up
+
+INDEX=/Volumes/TIGERII/nobackup/refseq/starindex/
+FASTA=/Volumes/TIGERII/nobackup/refseq/fasta/GRCm38.fasta
+GTF=/Volumes/TIGERII/nobackup/refseq/gtf/GRCm38.gtf
+
+CPU=3
+
+### Generate reference index
+
+STAR --runMode genomeGenerate --runThreadN $CPU --genomeDir $INDEX --genomeFastaFiles $FASTA  --sjdbGTFfile $GTF
