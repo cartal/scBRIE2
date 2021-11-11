957535e38e1606178652ae29ede21468630b57d8
cartal <cntalaveralopez@icloud.com>
2021-11-11 15:43:19 +0100

    fix issue with reads and added gunzip decompression

--- scripts/map_ss2_reads_STARsolo.sh
+++ scripts/map_ss2_reads_STARsolo.sh
@@ -4,7 +4,7 @@
 
 ### Set up 
 
-CPU=8
+CPU=3
 INDEX=/Volumes/TIGERII/nobackup/refseq/starindex/
 READS=/Users/ctl/github/scBRIE2/data/
 MANIFEST=/Users/ctl/github/scBRIE2/data/manifest.tsv
@@ -20,7 +20,14 @@ do
 
     cd $i 
 
-    STAR --runThreadN $CPU --genomeDir $INDEX --readFilesIn $READS/$i\_R1.fastq.gz $READS/$i\_R2.fastq.gz --soloType SmartSeq --readFilesManifest $MANIFEST --soloUMIdedup Exact --soloStrand Unstranded --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $i
+    cp $READS/$i\_R1.fastq.gz ./
+    cp $READS/$i\_R2.fastq.gz ./
+
+    gunzip $i\_R1.fastq.gz $i\_R2.fastq.gz
+
+    STAR --runThreadN $CPU --genomeDir $INDEX --readFilesIn $i\_R1.fastq $i\_R2.fastq --soloType SmartSeq --readFilesManifest $MANIFEST --soloUMIdedup Exact --soloStrand Unstranded --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $i
+
+    rm *.fastq 
 
     cd ..
 
