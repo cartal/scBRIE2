32d704693629d793fe0f4ca353f00b0cdf0d4515
cartal <cntalaveralopez@icloud.com>
2021-11-11 16:30:00 +0100

    Testing renaming of PE reads

--- scripts/map_ss2_reads_STARsolo.sh
+++ scripts/map_ss2_reads_STARsolo.sh
@@ -6,7 +6,7 @@
 
 CPU=24
 INDEX=/Volumes/TIGERII/nobackup/refseq/starindex/
-READS=/Users/ctl/github/scBRIE2/data/
+READS=/Users/ctl/github/scBRIE2/data
 MANIFEST=/Users/ctl/github/scBRIE2/data/manifest.tsv
 
 ### Run mapping 
@@ -19,12 +19,13 @@ do
 
     cd $i 
 
-    cp $READS/$i\_R1.fastq.gz ./
-    cp $READS/$i\_R2.fastq.gz ./
+    cp $READS/$i\_R1.fastq.gz pair_1.fastq.gz
+    cp $READS/$i\_R2.fastq.gz pair_2.fastq.gz
 
-    gunzip $i\_R1.fastq.gz $i\_R2.fastq.gz
+    gunzip pair_1.fastq.gz 
+    gunzip pair_2.fastq.gz
 
-    STAR --runThreadN $CPU --genomeDir $INDEX --readFilesIn $i\_R1.fastq $i\_R2.fastq --soloType SmartSeq --readFilesManifest $MANIFEST --soloUMIdedup Exact --soloStrand Unstranded --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $i
+    STAR --runThreadN $CPU --genomeDir $INDEX --readFilesIn pair_1.fastq pair_2.fastq --soloType SmartSeq --readFilesManifest $MANIFEST --soloUMIdedup Exact --soloStrand Unstranded --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $i
 
     rm *.fastq 
 
