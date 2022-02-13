a54acbb2f9c9aeab587e0b44b10f0f4f171ac6df
cartal <cntalaveralopez@icloud.com>
2022-02-13 18:23:36 +0100

    Updated script

--- scripts/2-brie-count/run_BRIE2_count-quant_pipeline.sh
+++ scripts/2-brie-count/run_BRIE2_count-quant_pipeline.sh
@@ -14,7 +14,3 @@ brie-count -a $GTF -S $SAMPLES -o $OUT -p $CPU
 ### Run Mode2-quant
 
 brie-quant -i $OUT/brie_count.h5ad -o $OUT/brie_quant_aggr.h5ad --interceptMode gene --minCount=2 --minUniqCount=2 --minCell=2
-
-### Run Mode2-diff
-
-#brie-quant -i $OUT/brie_count.h5ad -o $OUT/brie_quant_cell.h5ad -c $DATA_DIR/cell_info.tsv --interceptMode gene --LRTindex=All
